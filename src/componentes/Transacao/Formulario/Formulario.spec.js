a1b5051cad0d4384e41154023fec95a651416eb8
Vitor Soares <vitorsoaresf@gmail.com>
2023-08-12 11:20:39 -0300

    feat: implements test checkbox and select

--- src/componentes/Transacao/Formulario/Formulario.spec.js
+++ src/componentes/Transacao/Formulario/Formulario.spec.js
@@ -26,4 +26,66 @@ describe('Deve renderizar um campo de input', () => {
 
     expect(campoTexto).toHaveValue(50);
   });
+
+  test(`Deve chamar o evento de onSubmit ao clicar em realizar transacao`, () => {
+    // realizamos na linha abaixo o mock da funcao realizartransacao que no seu componente de origem possui um
+    //comportamento especifico, mas que aqui nao podera realizar
+
+    const realizarTransacao = jest.fn();
+
+    render(<Formulario realizarTransacao={realizarTransacao} />);
+
+    const btSubmit = screen.getByRole(`button`);
+
+    userEvent.click(btSubmit);
+
+    // No minimo foi chamada uma vez a fn abaixo
+    expect(realizarTransacao).toHaveBeenCalledTimes(1);
+
+    // funcao abaixo especifica se a fn realizarTransacao foi chamada com algum parametro especifico
+    // expect(realizarTransacao).toHaveBeenCalledWith(`nome do parametro`);
+  });
+
+  // testes para <select>
+  test('Deve selecionar a opcao de transacao ou deposito', () => {
+    const realizarTransacao = jest.fn();
+
+    render(<Formulario realizarTransacao={realizarTransacao} />);
+
+    userEvent.selectOptions(screen.getByTestId('select-opcoes'), ['Depósito']);
+
+    expect(screen.getByRole('option', { name: 'Depósito' }).selected).toBe(
+      true,
+    );
+
+    expect(screen.getByRole('option', { name: 'Transferência' }).selected).toBe(
+      false,
+    );
+  });
 });
+
+/*
+Metodos principais para o userEvent
+
+- click(): Dispara um evento de clique em um elemento;
+- dblClick: Dispara um evento de double click em um elemento;
+- type(): Escreve um texto dentro de um elemento de <input /> ou <textarea/>;
+- keyboard(): Simula eventos de teclado;
+- selectOptions(): Seleciona as opções especificadas de um <select /> ou <select multiple/>.
+
+
+Exemplo de teste com checkbox
+
+teste unitario
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Checkbox from './index';
+
+test('Deve renderizar um checkbox que pode ser clicado', () => {
+  render(<Checkbox />);
+  const input = screen.getByLabelText('Check');
+  userEvent.click(input);
+  expect(input).toBeChecked();
+});
+
+ */
